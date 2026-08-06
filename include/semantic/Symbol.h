01c142c568fd8acfea4c230a2503bb37ebf82108
Kevin <kevin.wirya.valerian@gmail.com>
2026-08-06 13:04:41 +0700

    feat: add symbol and symbol table header

---
+++ include/semantic/Symbol.h
@@ -0,0 +1,28 @@
+#ifndef SYMBOL_H
+#define SYMBOL_H
+
+#include <string>
+#include <vector>
+#include "ArrayDeclNode.h"
+
+enum class SymbolKind{
+    VARIABLE,
+    ARRAY,
+    FUNCTION,
+    SUBROUTINE
+};
+
+struct Symbol{
+    std::string name;
+    std::string type;
+    SymbolKind kind;
+    std::vector<ArrayDimension> dimensions;
+    std::vector<std::string> param_types;
+    bool is_common=false;
+    std::string common_block_name="";
+    Symbol()=default;
+    Symbol(const std::string& n,const std::string& t, SymbolKind k)
+        :name(n),type(t),kind(k){}
+};
+
+#endif
---
+++ include/semantic/SymbolTable.h
@@ -0,0 +1,31 @@
+#ifndef SYMBOL_TABLE_H
+#define SYMBOL_TABLE_H
+
+#include "Symbol.h"
+#include <unordered_map>
+#include <memory>
+#include <vector>
+#include <string>
+
+class Scope{
+    public:
+        std::unordered_map<std::string, Symbol> symbols;
+        std::shared_ptr<Scope> parent;
+        Scope(std::shared_ptr<Scope> p=nullptr): parent(p){}
+};
+
+class SymbolTable{
+    private:
+        std::shared_ptr<Scope> current_scope;
+        std::shared_ptr<Scope> global_scope;
+    public:
+        SymbolTable();
+        void enterScope();
+        void exitScope();
+        bool declare(const Symbol& sym);
+        const Symbol* lookup(const std::string& name)const;
+        const Symbol* lookupLocal(const std::string& name)const;
+        bool isGlobalScope()const;
+};
+
+#endif
